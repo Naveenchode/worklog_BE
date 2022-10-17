62a5d0824b4b4db34aa28c5c0a689dc00700bc06
Ch Naveen <naveenchode9@gmail.com>
2022-10-17 10:43:27 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const bodyparser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const route = require('./routes/index');
+//const multer =require('multer');
+
+
+
+const app = express();
+app.use(cookieParser());
+app.use(helmet());
+app.use(cors());
+app.options('*',cors());
+
+
+
+
+
+app.use(bodyparser.json());
+app.use('/',route);
+const port = process.env.port || 3000;
+app.listen(port, ()=>{
+  console.log(`App running on port ${port}....`);
+
+  /**
+ * @swagger
+ * /books:
+ *   get:
+ *     description: Get all books
+ *     responses:
+ *       200:
+ *         description: Success
+ * 
+ * 
+ */
+app.get('/books', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      title: "Harry Potter",
+    }
+  ])
+});
+
+
+
+});
+
+
+module.exports = app
